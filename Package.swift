43986b97444356b179afba1b03511431036aa7da
Dev <31865236+just-software-dev@users.noreply.github.com>
2025-01-11 02:37:48 -0300

    versions -> branches

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/apple/swift-crypto.git", "1.0.0" ..< "3.0.0"),
         .package(url: "https://github.com/GigaBitcoin/secp256k1.swift.git", exact: .init(0, 10, 0)),
-        .package(url: "https://github.com/just-software-dev/HsCryptoKit.Swift.git", .upToNextMinor(from: "1.3.0")),
+        .package(url: "https://github.com/just-software-dev/HsCryptoKit.Swift.git", branch: "main"),
     ],
     targets: [
         .target(
